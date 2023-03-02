bdc3cf97689f6cb26a86833959b41a87001308f4
Adam Norton <adam3norton@gmail.com>
2023-03-01 23:55:38 -0700

    Add project files.

---
+++ Mission9-anorto/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mission9_anorto.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
